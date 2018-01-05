0cb469766a6a3e630ece0ee759c31a7c6b7399d5
hsqzyswoai <55042903@qq.com>
2018-01-05 10:05:42 +0800

    CAR

--- zys1/src/main/java/com/zys/pojo/Car.java
+++ zys1/src/main/java/com/zys/pojo/Car.java
@@ -2,4 +2,12 @@ package com.zys.pojo;
 
 public class Car {
     private String CarName;
+
+    public String getCarName() {
+        return CarName;
+    }
+
+    public void setCarName(String carName) {
+        CarName = carName;
+    }
 }
